8cf65c1e008af563a96b5f43a08bb2d668250f0b
alevani <vanini.alexandre@gmail.com>
2020-10-14 10:26:07 +0200

    New code

--- MA2/mtcarlo.py
+++ MA2/mtcarlo.py
@@ -1,9 +1,13 @@
 from shapely.geometry import LinearRing, LineString, Point, Polygon
-from numpy import sin, cos, pi, sqrt, subtract, std, mean
+from numpy import sin, cos, pi, sqrt, subtract, std, mean, array
+# from adafruit_rplidar import RPLidar
+import threading
+from threading import Thread
 import matplotlib.pyplot as plt
 from math import floor
 from random import *
 import shapely
+import sys
 
 
 class ParticleFiltering:
@@ -13,33 +17,58 @@ class ParticleFiltering:
 
         # the world is a rectangular arena with width W and height H
         self.world = LinearRing(
-            [(W/2, H/2), (-W/2, H/2), (-W/2, -H/2), (W/2, -H/2)])
+            [(self.W/2, self.H/2), (-self.W/2, self.H/2), (-self.W/2, -self.H/2), (self.W/2, -self.H/2)])
 
         self.nb_samples = 200
         self.nb_candidates = 10
+        self.scan_data = [0]*360
 
         # Create nb_samples samples
         self.samples = [[uniform(-.96, .96), uniform(-0.58, .58), randint(0, 360)]
-                        for _ in range(0, nb_samples)]
+                        for _ in range(0, self.nb_samples)]
 
         self.best_candidates = self.get_best_candidates(self.samples)
         # [(plt.scatter(be[1][0], be[1][1], c='b')) for be in best_candidates]
+
+        self.x = 0.0
+        self.y = 0.0
+        self.angle = 0
+        self.position = (0, 0, 0)
+
+    #     # Setup the RPLidar
+    #     PORT_NAME = '/dev/ttyUSB0'
+    #     self.lidar = RPLidar(None, PORT_NAME)
+
+    #     print('start lidar scan thread')
+    #     self.scanner_thread = threading.Thread(target=self.lidarScan)
+    #     self.scanner_thread.daemon = True
+    #     self.scanner_thread.start()
+
+    # def lidarScan(self):
+    #     print("Starting background lidar scanning")
+    #     for scan in self.lidar.iter_scans():
+    #         for (_, angle, distance) in scan:
+    #             self.scan_data[min([359, floor(angle)])] = distance
+
+    def set_xya(self, x, y, a):
+        self.x = x
+        self.y = y
+        self.angle = a
 
     # return the average position of the robot on the map
     def avg_xya(self, os):
         return (mean([x[1][0] for x in os]), mean([x[1][1]
                                                    for x in os]), mean([x[1][2] for x in os]))
 
-    def get_robot_angle_step(self):
-        return 20
-
     # Fitness function
     def calculate_distance(self, a, b):
         return sum(abs(subtract(a, b)))
+        # return sum(abs(array([a**2 if a < 0.02 and a > -0.02 else a for a in subtract(a, b)])))
 
     # Return world intersection with a ray
     def return_inter(self, alpha, x, y):
-        ray = LineString([(x, y), (x+cos(alpha)*2*W, (y+sin(alpha)*2*H))])
+        ray = LineString(
+            [(x, y), (x+cos(alpha)*2*self.W, (y+sin(alpha)*2*self.H))])
         s = self.world.intersection(ray)
         return sqrt((s.x-x)**2+(s.y-y)**2)
 
@@ -47,56 +76,44 @@ class ParticleFiltering:
     def get_simulated_lidar_values(self, x):
         # shift of alpha so that both the robot and the simulation look in the same direction
         # ? - or + shift
-        return [return_inter((x[2] + (alpha * 36)) % 360, x[0], x[1]) for alpha in range(0, 10)]
+        return [self.return_inter((x[2] + (alpha * 36)) % 360, x[0], x[1]) for alpha in range(0, 10)]
 
     # Return the values thymio lidar sensor
     def get_lidar_values(self):
-        #! Dummy values
-        x_start = 0.0
-        y_start = 0.0
-        a_start = 0.0
-        plt.scatter(x_start, y_start, c='g')
-
-        # Set the robot position at 0 to see if it works
-        return get_simulated_lidar_values([x_start, y_start, a_start])
-        # return [uniform(0.01, 1.94) for x in range(0, 10)]
+        return self.get_simulated_lidar_values((0, 0, 0))
+        # return [self.scan_data[x] / 1000 for x in list(range(0, 360, 36))]
 
     # Return the nb_candidates best candidates
     def get_best_candidates(self, samples):
-        # Contains a list of nb_samples lidar simlutation (each with 10 rays)
-        lr_samples = [(get_simulated_lidar_values(x), x) for x in samples]
+            # Contains a list of nb_samples lidar simlutation (each with 10 rays)
+        lr_samples = [(self.get_simulated_lidar_values(x), x) for x in samples]
 
         DISTrr = self.get_lidar_values()
 
-        candidates = [((calculate_distance(DISTrr, sample[0]), sample[1]))
+        candidates = [((self.calculate_distance(DISTrr, sample[0]), sample[1]))
                       for sample in lr_samples]
 
         # # sort by second index
         candidates.sort(key=lambda candidates: candidates[0])
 
-        return candidates[: nb_candidates]
+        return candidates[: self.nb_candidates]
 
-    def get_x_y_alpha(self):
-        iter = 0
-        while iter != 10:
-            iter += 1
+    def Localize(self):
+        while True:
 
-            #! wait for step()?
             # Move and Resample
             re_candidates = []
             for b in self.best_candidates:
-                # b[1][0] += 0.01  # need to move according to angle ;(
-                # b[1][1] += 0.01
-                # b[1][2] += get_robot_angle_step()
-                b[1][0] += 0.0  # need to move according to angle ;(
-                b[1][1] += 0.0
-                b[1][2] += 0
+                b[1][0] += self.x
+                b[1][1] += self.y
+                b[1][2] += self.angle
 
                 # Creates nb_candidates new subsample for each best candidate
                 for _ in range(0, self.nb_candidates):
                     # Resample around a 1 centimer wide box.
                     x = b[1][0] + uniform(-0.01, 0.01)
                     y = b[1][1] + uniform(-0.01, 0.01)
+                    a = (b[1][2] + randint(-10, 10)) % 360
 
                     # Keeps the resampling in the box
                     if x > .96:
@@ -109,51 +126,25 @@ class ParticleFiltering:
                     elif y < -.58:
                         y = -.58
 
-                    re_candidates.append([x, y, randint(0, 360)])
+                    re_candidates.append([x, y, a])
 
             self.best_candidates = self.get_best_candidates(re_candidates)
-        return self.avg_xya(self.best_candidates)
+            self.position = self.avg_xya(self.best_candidates)
 
 
-# iter = 0
-# while iter != 10:
-#     iter += 1
+if __name__ == '__main__':
+    pf = ParticleFiltering()
 
-#     # Move and Resample
-#     re_candidates = []
-#     for b in best_candidates:
-#         # b[1][0] += 0.01  # need to move according to angle ;(
-#         # b[1][1] += 0.01
-#         # b[1][2] += get_robot_angle_step()
-#         b[1][0] += 0.0  # need to move according to angle ;(
-#         b[1][1] += 0.0
-#         b[1][2] += 0
+    print('start particle filtering thread')
+    thread = Thread(target=pf.Localize)
+    thread.daemon = True
+    thread.start()
+    pf.set_xya(0, 0, 0)
+    try:
+        while True:
+            print(pf.position)
 
-#         # Creates nb_candidates new subsample for each best candidate
-#         for _ in range(0, nb_candidates):
-#             # Resample around a 1 centimer wide box.
-#             x = b[1][0] + uniform(-0.01, 0.01)
-#             y = b[1][1] + uniform(-0.01, 0.01)
+    except KeyboardInterrupt:
+        sys.exit()
 
-#             # Keeps the resampling in the box
-#             if x > .96:
-#                 x = .96
-#             elif x < -.96:
-#                 x = -.96
-
-#             if y > .58:
-#                 y = .58
-#             elif y < -.58:
-#                 y = -.58
-
-#             re_candidates.append([x, y, randint(0, 360)])
-
-#     best_candidates = get_best_candidates(re_candidates)
-    # [(plt.scatter(be[1][0], be[1][1], c='r')) for be in best_candidates]
-
-
-# print(avg_xya(best_candidates))
-# [(plt.scatter(be[1][0], be[1][1], c='r')) for be in best_candidates]
-# plt.show()
-
-# TODO use the color sensor afterward
+    # TODO use the color sensor afterward
--- MA2/robot_dance.py
+++ MA2/robot_dance.py
@@ -5,6 +5,7 @@ from threading import Thread
 from time import sleep
 import threading
 from random import randint
+from numpy import arctan
 import os
 from mtcarlo import *
 
@@ -15,16 +16,13 @@ os.system("(asebamedulla ser:name=Thymio-II &) && sleep 0.3")
 
 class Thymio:
     def __init__(self):
-        self.aseba = self.setup()
-
-    def setup(self):
         print("Setting up")
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         bus = dbus.SessionBus()
         self.asebaNetworkObject = bus.get_object("ch.epfl.mobots.Aseba", "/")
 
         self.asebaNetwork = dbus.Interface(
-            asebaNetworkObject, dbus_interface="ch.epfl.mobots.AsebaNetwork"
+            self.asebaNetworkObject, dbus_interface="ch.epfl.mobots.AsebaNetwork"
         )
         # load the file which is run on the thymio
         self.asebaNetwork.LoadScripts(
@@ -33,14 +31,26 @@ class Thymio:
 
         # give th robot a gender, a change its color accordingly
         self.gender = self.selectGender()
-        self.aseba.SendEventName(
-            "led.top", (255, 0, 0) if gender else (0, 0, 255))
+        self.asebaNetwork.SendEventName(
+            "led.top", (255, 0, 0) if self.gender else (0, 0, 255))
 
         self.confidence = 0
-        self.confidence()
+        self.growConfidence()
 
-        # scanning_thread = Process(target=robot.drive, args=(200,200,))
-        return asebaNetwork
+        self.particleFilter = ParticleFiltering()
+        self.particleFilter.set_xya(0, 0, 0)
+
+        print('start particle filtering thread')
+        self.thread = Thread(target=self.particleFilter.Localize)
+        self.thread.daemon = True
+        self.thread.start()
+
+        self.dancefloor = [(.4, .3), (.4, -.3), (-.4, .3),
+                           (-.4, -.3)]  # dancefloor position
+        self.aseba = self.asebaNetwork
+
+    def setup(self):
+        return self.asebaNetwork
 
     def stopAsebamedulla(self):
         os.system("pkill -n asebamedulla")
@@ -54,9 +64,14 @@ class Thymio:
         return randint(0, 1)
 
     # Periodically increase confidence
-    def confidence(self):
+    def growConfidence(self):
         self.confidence += 1
-        threading.Timer(2, confidence).start()
+        # rx = self.receiveInformation()
+        rx = 1
+        if not rx == 0:  # ! change on depending what RX receive
+            threading.Timer(2, self.growConfidence).start()
+        else:
+            self.dance(rx)
 
     def getConfidence(self):
         return self.confidence
@@ -64,24 +79,27 @@ class Thymio:
     def resetConfidence(self):
         self.confidence = 0
 
-    def startCommunication(self):
-        # this enables the prox.com communication channels
-        self.asebaNetwork.SendEventName("prox.comm.enable", [1])
-        # This enables the prox.comm rx value to zero, gets overwritten when receiving a value
-        self.asebaNetwork.SendEventName("prox.comm.rx", [0])
+    # def startCommunication(self):
+    #     # this enables the prox.com communication channels
+    #     self.asebaNetwork.SendEventName("prox.comm.enable", [1])
+    #     # This enables the prox.comm rx value to zero, gets overwritten when receiving a value
+    #     self.asebaNetwork.SendEventName("prox.comm.rx", [0])
 
-    # Remeber to change tx number when finding a partner
-    def sendInformation(self, number):
-        self.asebaNetwork.SendEventName("prox.comm.tx", [number])
+    # # Remeber to change tx number when finding a partner
+    # def sendInformation(self, number):
+    #     self.asebaNetwork.SendEventName("prox.comm.tx", [number])
 
-    # Remeber to change rx number after confirming a partner. This can be done the same way as the tx :)
-    def receiveInformation(self):
-        self.rx = asebaNetwork.GetVariable("thymio-II", "prox.comm.rx")
-        print(rx[0])
+    # # Remeber to change rx number after confirming a partner. This can be done the same way as the tx :)
+    # def receiveInformation(self):
+    #     self.rx = asebaNetwork.GetVariable("thymio-II", "prox.comm.rx")
+    #     return rx[0]
 
     # Move the robot
-    def step(self, left, right):
+    def step(self, left, right, angle):
+        # self.particleFilter.set_xya(left, right, angle)
         self.aseba.SendEventName("motor.target", [left, right])
+        sleep(10)
+        self.stop()
 
     # Stop the robot's motion
     def stop(self):
@@ -90,31 +108,64 @@ class Thymio:
         self.aseba.SendEventName("motor.target", [left_wheel, right_wheel])
 
     def wander(self):
+        pos = self.particleFilter.position
+        lidar = self.particleFilter.get_lidar_values
+        #     # self.rest()
+
+    def dance(self, d):
+        #! goto dancefloor
+        dfPos = self.dancefloor[d]
+        self.stop()
         pass
 
-    def dance(self):
-        self.aseba.SendEventName(
-            "led.top", (255, 0, 255))
+    def find_angle(self, pos1, pos2):
+        dy = abs(pos2[1] - pos1[1])
+        dx = abs(pos2[0] - pos1[0])
+        alpha = arctan(dy/dx)
+        return 180 - alpha - pos1[2]
+
+    def rotate(self):
+        # TODO move robot from 1 degree
+        pos = self.particleFilter.position
+        pos[2] += 1
+        self.particleFilter.set_xya(pos)
+
+    def forward(self):
+        # TODO forward from .5 centimeter?
+        pos = self.particleFilter.position
+        pos[0] += 1  # TODO calculate with angle, +1 is wrong
+        pos[1] += 1  # TODO calculate with angle, +1 is wrong
+        self.particleFilter.set_xya(pos)
+
+    def goto(self, x, y):
+        pos = self.particleFilter.position
+        rotation = self.find_angle(pos, (x, y))
+
+        while pos[2] != rotation:
+            pos = self.particleFilter.position
+            self.rotate()
+        while (pos[0], pos[1]) != (x, y):
+            pos = self.particleFilter.position
+            self.forward()
+
+
+def dance(self):
+        # Robot become purple
+    self.aseba.SendEventName(
+        "led.top", (255, 0, 255))
 
 
 if __name__ == '__main__':
     rest = False
     try:
-        l = ParticleFiltering()
-        pos = l.get_x_y_alpha()
         robot = Thymio()
+        robot.step(200, 200, 0)
         while not rest:
-            if robot.getConfidence() > 10:
-                robot.resetConfidence()
-                robot.wander()
-                robot.dance()
-                robot.rest()
-                rest = True
-
-        # Put the sens method in a thread and run it
-        # thread = Thread(target=robot.sens)
-        # thread.daemon = True
-        # thread.start()
+            pass
+            # if robot.getConfidence() > 10:
+            #     robot.resetConfidence()
+            #     # robot.wander()
+            #     rest = True
 
     except KeyboardInterrupt:
         print("Stopping robot")
