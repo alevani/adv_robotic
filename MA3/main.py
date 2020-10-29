145e2e44a518f7ae52f74d0efb27afb3c91e6682
alevani <vanini.alexandre@gmail.com>
2020-10-29 13:11:35 +0100

    Basic functions

---
+++ MA3/main.py
@@ -0,0 +1,22 @@
+class State:
+    def __init__(self):
+        super().__init__()
+
+
+class Action:
+    def __init__(self):
+        super().__init__()
+
+
+def back_propagation():
+    pass
+
+
+# Reward function
+def Q(state, action):
+    # reward forever after
+    reward = 0
+    return reward
+
+
+reward = Q(State(), Action())
