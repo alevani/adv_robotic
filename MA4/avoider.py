3240a13e5296e89d9e741fc1a5ca6897cf6e4a2d
alevani <vanini.alexandre@gmail.com>
2020-11-16 09:38:53 +0100

    base for ma4

---
+++ MA4/avoider.py
@@ -0,0 +1,14 @@
+'''
+SPRINT 1
+- Basic requirements (LEDs.. Comm..)
+- Random walk in the arena
+- Behaviour based wall avoidance
+- Behaviour based safe zone detection
+
+SPRINT 2
+- safe zone color detection
+- seeker avoidance color based
+
+SPRINT 3
+- Learning?
+'''
